95a51b1e91ff74039b17d268f446cbcccbe47453
eliran <eliranmal@gmail.com>
2021-12-04 23:30:46 +0200

    feat: allow decoration in useAnimationFrame
    allow to decorate the tick function, e.g. for use with mobx.action

--- src/hooks/useAnimationFrame.js
+++ src/hooks/useAnimationFrame.js
@@ -3,17 +3,18 @@ import {useEffect} from 'react'
 
 export const useAnimationFrame = (
   tickCallback = () => {},
-  stopPredicate = () => 1
+  stopPredicate = () => 1,
+  tickDecorator = fn => fn
 ) => {
   useEffect(() => {
     let reqId
-    const tick = () => {
+    const tick = tickDecorator(() => {
       if (stopPredicate()) {
         return
       }
       tickCallback()
       reqId = window.requestAnimationFrame(tick)
-    }
+    })
     reqId = window.requestAnimationFrame(tick)
     return () => {
       window.cancelAnimationFrame(reqId)
